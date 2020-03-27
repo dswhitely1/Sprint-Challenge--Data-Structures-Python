f3b634fc599f40ddb754920e70e8b987f4bc12b9
Donald Whitely <dswhitely1@gmail.com>
2020-03-27 12:01:33 -0400

    Test passing for Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,14 +8,35 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            # update the value of the self_current node
+            self.current.value = item
+            if self.current.next is None:
+                while self.current.prev is not None:
+                    self.current = self.current.prev
+            else:
+                self.current = self.current.next
+            # Need to find oldest and replace with new value
+        # Set self.current to the first oldest.
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+        if len(self.storage) < self.capacity and self.current is None:
+            self.current = self.storage.head
+
+
+        # if storage is at capacity
+        # Need to remove the oldest
+
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
-
+        cur_list = self.storage.head
+        while cur_list is not None and cur_list.next is not None:
+            list_buffer_contents.append(cur_list.value)
+            cur_list = cur_list.next
+        if cur_list:
+            list_buffer_contents.append(cur_list.value)
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
@@ -30,3 +51,24 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
+
+buffer = RingBuffer(3)
+
+print(buffer.get())   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+print(buffer.get())   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+
+print(buffer.get())   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+print(buffer.get())   # should return ['d', 'e', 'f']
