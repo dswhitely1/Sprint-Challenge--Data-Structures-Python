42ed0653175bc13dc0fd07c51d8189eca932eb75
Donald Whitely <dswhitely1@gmail.com>
2020-03-27 12:28:20 -0400

    Duplicates returning in 0.12 seconds

--- names/names.py
+++ names/names.py
@@ -13,10 +13,67 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+def merge(a, b):
+    x = y = 0
+    result = []
+
+    while x < len(a) and y < len(b):
+        if a[x] < b[y]:
+            result.append(a[x])
+            x += 1
+        else:
+            result.append(b[y])
+            y += 1
+
+    result.extend(a[x:])
+    result.extend(b[y:])
+
+    return result
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    midpoint = len(arr) // 2
+    lhs = merge_sort(arr[midpoint:])
+    rhs = merge_sort(arr[:midpoint])
+
+    return merge(lhs, rhs)
+
+
+print('Starting Sort of Names_2')
+sort_start = time.time()
+sorted_names = merge_sort(names_2)
+sort_end = time.time()
+print(f'Sorting finished in {sort_end - sort_start}')
+
+
+def binary_search(target, arr=sorted_names):
+    if len(arr) == 0:
+        return False
+
+    low = 0
+    high = len(arr) - 1
+    found = False
+    while low <= high and not found:
+        middle = (low + high) // 2
+        if arr[middle] == target:
+            return True
+        elif arr[middle] < target:
+            low = middle + 1
+        else:
+            high = middle - 1
+
+    return False
+
+
+for name in names_1:
+    if binary_search(name):
+        duplicates.append(name)
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -53,22 +53,22 @@ class ArrayRingBuffer:
         pass
 
 
-buffer = RingBuffer(3)
-
-print(buffer.get())   # should return []
-
-buffer.append('a')
-buffer.append('b')
-buffer.append('c')
-
-print(buffer.get())   # should return ['a', 'b', 'c']
-
-# 'd' overwrites the oldest value in the ring buffer, which is 'a'
-buffer.append('d')
-
-print(buffer.get())   # should return ['d', 'b', 'c']
-
-buffer.append('e')
-buffer.append('f')
-
-print(buffer.get())   # should return ['d', 'e', 'f']
+# buffer = RingBuffer(3)
+#
+# print(buffer.get())   # should return []
+#
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+#
+# print(buffer.get())   # should return ['a', 'b', 'c']
+#
+# # 'd' overwrites the oldest value in the ring buffer, which is 'a'
+# buffer.append('d')
+#
+# print(buffer.get())   # should return ['d', 'b', 'c']
+#
+# buffer.append('e')
+# buffer.append('f')
+#
+# print(buffer.get())   # should return ['d', 'e', 'f']
